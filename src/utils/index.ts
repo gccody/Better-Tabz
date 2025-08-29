2cae32364a6d7926d33f5b22ec8d47344cc22817
Grant <gccody2010@live.com>
2025-08-28 23:22:53 -0400

    fix: Fixed not being able to find others folder

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -13,8 +13,8 @@ export const getBookmarks = (): Promise<BookmarkTreeNode[]> =>
     const otherBookmarksTitle = typeof firefox === "undefined" ? "Other bookmarks" : "Other Bookmarks";
     const dataFolderTitle = "BetterTabz";
     const tree = (await browser.bookmarks.getTree())[0];
-    const otherBookmarks = tree.children?.find((val) => val.title === otherBookmarksTitle) ?? await browser.bookmarks.create({ parentId: tree.id, title: otherBookmarksTitle });
-    const dataFolder = otherBookmarks.children?.find((val => val.title === dataFolderTitle)) ?? await browser.bookmarks.create({ parentId: otherBookmarks.id, title: dataFolderTitle });
+    const otherBookmarks = tree.children?.find((val) => val.title.toLowerCase() === otherBookmarksTitle.toLowerCase())  ?? await browser.bookmarks.create({ parentId: tree.id, title: otherBookmarksTitle });
+    const dataFolder = otherBookmarks.children?.find((val => val.title.toLowerCase() === dataFolderTitle.toLowerCase())) ?? await browser.bookmarks.create({ parentId: otherBookmarks.id, title: dataFolderTitle });
 
     resolve(dataFolder.children ?? []);
   });
