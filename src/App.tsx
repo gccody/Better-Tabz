a84121d2e22da72e2ead2c1d38098a03bb88543c
Grant <gccody2010@live.com>
2025-08-28 23:27:17 -0400

    chore: Added text to show that you need to manually add data for now

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,16 @@ function App() {
 
   return (
     <div className="flex flex-row w-dvw h-dvh gap-2 p-2" style={{ backgroundColor: "#2d2e31" }}>
-      {
+      {bookmarks.length === 0 ? (
+        <div className="flex items-center justify-center w-full h-full text-center text-white">
+          <div>
+            <p className="text-lg mb-2">You need to add some folders to the BetterTabz folder</p>
+            <p>then add some bookmarks to those sub folders</p>
+          </div>
+        </div>
+      ) : (
         bookmarks.map((folder) => <Card folder={folder} />)
-      }
+      )}
     </div>
   )
 }
