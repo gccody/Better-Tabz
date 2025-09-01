6db23c6191e1481ac3614db949e884917e2e38c5
Grant <gccody2010@live.com>
2025-08-31 21:58:29 -0400

    change: Changed add buttom from color transition to opacity transition

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
         bookmarks.map((folder) => <Card folder={folder} />)
       )}
       <button
-        className="fixed bottom-4 left-4 bg-blue-600 hover:bg-blue-700 text-white p-3 rounded-full shadow-lg transition-colors"
+        className="fixed bottom-4 left-4 bg-blue-600 text-white p-3 rounded-full shadow-lg hover:opacity-100 opacity-50 transition-opacity"
         onClick={handleAddCard}
       >
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
